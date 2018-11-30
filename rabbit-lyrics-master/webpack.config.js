df0efa1507877cd185f5fcc0b911fae55aad125a
flaregunn <45474133+flaregunn@users.noreply.github.com>
2018-11-29 23:26:26 -0500

    Add files via upload

---
+++ rabbit-lyrics-master/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "rabbit-lyrics.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
